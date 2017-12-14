be8040f616b3fc963ec99bf185be35864320117c
Alireza Borhan <alirezamborhan@gmail.com>
2017-12-14 18:58:25 +0330

    Add client.py, the menu file.

---
+++ client.py
@@ -0,0 +1,22 @@
+import signup
+import signin
+
+def main():
+    e = ""
+    while True:
+        print(e)
+        e = ""
+        print("What do you wish to do?\n")
+        print("1. Sign up")
+        print("2. Sign in")
+        print("3. Exit\n")
+        inp = str(input())
+        if inp == '1':
+            signup.main()
+        elif inp == '2':
+            signin.main()
+        elif inp == '3':
+            print("Fairwell!")
+            break
+        else:
+            e = "Enter 1, 2, or 3."
